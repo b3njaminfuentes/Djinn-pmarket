bc1b578891d47127c1bd8e83fd9492facf8828a2
Chrome <benjofuentesv14@gmail.com>
2026-02-24 11:39:53 -0300

    fix: duplicate motion button tag from previous styling

--- app/page.tsx
+++ app/page.tsx
@@ -377,7 +377,6 @@ export default function DjinnLanding() {
                             className="flex flex-col items-center"
                         >
                             <motion.button
-                                <motion.button
                                 initial={{ opacity: 0, y: 15 }}
                                 animate={{ opacity: 1, y: 0 }}
                                 transition={{ delay: 0.2 }}
